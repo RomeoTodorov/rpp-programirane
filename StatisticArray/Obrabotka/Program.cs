f80e8f7993100e02f416d7c9807da453cf0c00d0
RomeoTodorov <r.todorov2019@pmg-pernik.com>
2023-02-21 12:16:18 +0200

    add project

---
+++ StatisticArray/Obrabotka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obrabotka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
